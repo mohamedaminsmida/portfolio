165d09e7ece0c4c36380e916bf2245a0e5b12e9d
Yassine Kooli <yassine.kooli@satoripop.com>
2025-05-15 11:17:15 +0100

    add upwork

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare, FaTwitter } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useTranslation } from "@/lib/hooks/useTranslation";
@@ -107,7 +107,7 @@ export default function Intro() {
         <div className="flex gap-2">
           <a
             className="bg-white p-3 sm:p-4 text-gray-700 hover:text-primary-600 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 dark:hover:text-primary-400 dark:border-white/10"
-            href="https://linkedin.com/in/yassine-dev"
+            href="https://www.linkedin.com/in/yassine-kooli/"
             target="_blank"
             aria-label="LinkedIn Profile"
           >
@@ -116,7 +116,7 @@ export default function Intro() {
 
           <a
             className="bg-white p-3 sm:p-4 text-gray-700 hover:text-primary-600 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 dark:hover:text-primary-400 dark:border-white/10"
-            href="https://github.com/yassine-dev"
+            href="https://github.com/Yassine-Kooli"
             target="_blank"
             aria-label="GitHub Profile"
           >
@@ -125,11 +125,17 @@ export default function Intro() {
 
           <a
             className="bg-white p-3 sm:p-4 text-gray-700 hover:text-primary-600 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 dark:hover:text-primary-400 dark:border-white/10"
-            href="https://twitter.com/yassine_dev"
+            href="https://www.upwork.com/freelancers/~01e5595c71628fd231?viewMode=1"
             target="_blank"
-            aria-label="Twitter Profile"
+            aria-label="Upwork Profile"
           >
-            <FaTwitter className="text-lg" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="text-lg w-5 h-5 fill-current"
+            >
+              <path d="M18.561 13.158c-1.102 0-2.135-.467-3.074-1.227l.228-1.076.008-.042c.207-1.143.849-3.06 2.839-3.06 1.492 0 2.703 1.212 2.703 2.703-.001 1.489-1.212 2.702-2.704 2.702zm0-8.14c-2.539 0-4.51 1.649-5.31 4.366-1.22-1.834-2.148-4.036-2.687-5.892H7.828v7.112c-.002 1.406-1.141 2.546-2.547 2.548-1.405-.002-2.543-1.143-2.545-2.548V3.492H0v7.112c0 2.914 2.37 5.303 5.281 5.303 2.913 0 5.283-2.389 5.283-5.303v-1.19c.529 1.107 1.182 2.229 1.974 3.221l-1.673 7.873h2.797l1.213-5.71c1.063.679 2.285 1.109 3.686 1.109 3 0 5.439-2.452 5.439-5.45 0-3-2.439-5.439-5.439-5.439z" />
+            </svg>
           </a>
         </div>
       </motion.div>
